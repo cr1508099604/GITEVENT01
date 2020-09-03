b361a15d716457f8452b1d072fda32c47fed1232
cr1508099604 <1508099604@qq.com>
2020-09-03 14:43:23 +0800

    index over

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,20 @@ $.ajaxPrefilter(function (options) {
     //再发起真正的ajax请求前,同意拼接请求的根路径
     console.log(options)
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+    //统一为有权限的接口设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    //当请求结束后判断用户的设置访问权限
+    options.complete = function (res) {
+        //当用户身份认证失败后
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
+            //1.强制清空本地tonke
+            localStorage.removeItem('token')
+            //2.强制跳转登录页
+            location.herf = '/login.html'
+        }
+    }
 })
